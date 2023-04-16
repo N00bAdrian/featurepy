91266979d9a2eeb6e263621579f38daa63a7ad7a
N00bAdrian <adrianwong227@gmail.com>
2023-04-16 22:20:32 +0100

    feat: add message cards to base feature

--- flask_example/messageBoard/main.py
+++ flask_example/messageBoard/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap5
 from .views import home
 
 def create_app():
     app = Flask(__name__)
+    bootstrap = Bootstrap5(app)
 
     app.register_blueprint(home.bp)
 
--- flask_example/messageBoard/views/home.py
+++ flask_example/messageBoard/views/home.py
@@ -6,6 +6,25 @@ bp = Blueprint("home", __name__, template_folder="../templates")
 class HomeView(MethodView):
 
     def get(self):
-        return render_template("home.html")
+        messages = [
+            {
+                "title": "Title 1",
+                "message": "This is a message"
+            },
+            {
+                "title": "Title 2",
+                "message": "This is also a message"
+            },
+            {
+                "title": "Title 3",
+                "message": "This is also a message"
+            },
+            {
+                "title": "Title 4",
+                "message": "This is also a message"
+            },
+        ]
+
+        return render_template("home.html", messages=messages)
 
 bp.add_url_rule("/", view_func=HomeView.as_view("home"))
