199b3051f3d1e58e4b033a68cfe94c6ea3e8ac40
N00bAdrian <adrianwong227@gmail.com>
2023-04-16 22:48:52 +0100

    feat: add message form

--- flask_example/messageBoard/views/home.py
+++ flask_example/messageBoard/views/home.py
@@ -1,11 +1,18 @@
 from flask.views import MethodView
 from flask import Blueprint, render_template
+from wtforms import Form, StringField, TextAreaField
+from wtforms.validators import DataRequired
 
 bp = Blueprint("home", __name__, template_folder="../templates")
+
+class MessageForm(Form):
+    title = StringField("Title", validators=[DataRequired()])
+    message = TextAreaField("Message", validators=[DataRequired()])
 
 class HomeView(MethodView):
 
     def get(self):
+        form = MessageForm()
         messages = [
             {
                 "title": "Title 1",
@@ -25,6 +32,6 @@ class HomeView(MethodView):
             },
         ]
 
-        return render_template("home.html", messages=messages)
+        return render_template("home.html", form=form, messages=messages)
 
 bp.add_url_rule("/", view_func=HomeView.as_view("home"))
