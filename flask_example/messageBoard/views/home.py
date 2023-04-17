f777e3e529af76033f1b64d4ddf8a6650176f199
N00bAdrian <adrianwong227@gmail.com>
2023-04-17 12:47:05 +0100

    feat: complete base feature by adding form handling

--- flask_example/messageBoard/views/home.py
+++ flask_example/messageBoard/views/home.py
@@ -1,5 +1,5 @@
 from flask.views import MethodView
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from wtforms import Form, StringField, TextAreaField
 from wtforms.validators import DataRequired
 from sqlalchemy import select
@@ -16,10 +16,19 @@ class MessageForm(Form):
 class HomeView(MethodView):
 
     def get(self):
-        form = MessageForm()
+        form = MessageForm(request.form)
 
         messages = db.session.execute(select(Message)).scalars().all()
 
         return render_template("home.html", form=form, messages=messages)
+
+    def post(self):
+        form = MessageForm(request.form)
+        if form.validate():
+            print("Form validated")
+            db.session.add(Message(title=form.title.data, message=form.message.data))
+            db.session.commit()
+        
+        return redirect("/")
 
 bp.add_url_rule("/", view_func=HomeView.as_view("home"))
