a66ccae6997b8642bcc2865ecb24920c63c45f6e
N00bAdrian <adrianwong227@gmail.com>
2023-04-20 11:38:05 +0100

    feat: modify html files using beautifulsoup

--- flask_example/addAuthor/feature.py
+++ flask_example/addAuthor/feature.py
@@ -1,11 +1,30 @@
 from featuremonkey import compose
-from aspectlib import Aspect, weave, Proceed
+from flask import render_template_string
+from aspectlib import Aspect, weave, Proceed, Return
 from wtforms import StringField
+from bs4 import BeautifulSoup
+import os
+
 from messageBoard.db import db
 
 @Aspect
-def addAuthorCard(_, **kwargs):
-    yield Proceed("home_with_author.html", **kwargs)
+def addAuthorCard(filename, **kwargs):
+    with open(f'messageBoard/templates/{filename}') as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+
+        form = soup.find("form")
+        author_form = soup.new_tag("div", id="author", class_="mb-3")
+        author_form.string = """{{form.author.label(class_="form-label")}}{{form.author(class_="form-control")}}"""
+        form.find(id="title").insert_after(author_form)
+
+        card = soup.find("div", class_="card-body")
+        author_card = soup.new_tag("h6", class_="card-subtitle text-body-secondary")
+        author_card.string = "{{msg.author}}"
+        card.h5.insert_after(author_card)
+        
+        yield Return(render_template_string(soup.prettify(), **kwargs))
+
+    # yield Proceed("home_with_author.html", **kwargs)
 
 @Aspect
 def defaultAnon(self, res, **kwargs):
