8432145c29b210acd9a6eac98dfba5a5966685e8
N00bAdrian <adrianwong227@gmail.com>
2023-05-18 02:54:21 +0100

    feature: add FView and FMethodView for FOP

--- examples/flask_example/addAuthor/feature.py
+++ examples/flask_example/addAuthor/feature.py
@@ -1,42 +1,32 @@
-from flask import render_template
-from aspectlib import Aspect, weave, Proceed, Return
 from wtforms import StringField
 from bs4 import BeautifulSoup
 from tempfile import NamedTemporaryFile
 from os import path
+from flask import request
 
 from messageBoard.db import db
+from messageBoard.routes.home import MessageForm
 
-@Aspect
-def addAuthorCard(filename, **kwargs):
-    with open(f'messageBoard/templates/{filename}') as fp:
-        soup = BeautifulSoup(fp, 'html.parser')
+def add_author_form(soup):
+    form = soup.find("form")
+    author_form = soup.new_tag("div", attrs={
+        "id": "author", 
+        "class": "mb-3"
+    })
+    author_form.string = """{{form.author.label(class_="form-label")}}{{form.author(class_="form-control")}}"""
+    form.find(id="title").insert_after(author_form)
 
-        form = soup.find("form")
-        author_form = soup.new_tag("div", attrs={
-            "id": "author", 
-            "class": "mb-3"
-        })
-        author_form.string = """{{form.author.label(class_="form-label")}}{{form.author(class_="form-control")}}"""
-        form.find(id="title").insert_after(author_form)
+    return soup
 
-        card = soup.find("div", class_="card-body")
-        author_card = soup.new_tag("h6", attrs={
-            "class": "card-subtitle text-body-secondary"
-        })
-        author_card.string = "{{msg.author}}"
-        card.h5.insert_after(author_card)
-        
-        with NamedTemporaryFile(suffix=".html", dir="messageBoard/templates") as tfp:
-            tfp.write(str.encode(soup.prettify()))
-            tfp.seek(0)
-            yield Return(render_template(path.basename(tfp.name), **kwargs))
+def add_author_card(soup):
+    card = soup.find("div", class_="card-body")
+    author_card = soup.new_tag("h6", attrs={
+        "class": "card-subtitle text-body-secondary"
+    })
+    author_card.string = "{{msg.author}}"
+    card.h5.insert_after(author_card)
 
-@Aspect
-def defaultAnon(self, res, **kwargs):
-    if res.author == "":
-        res.author = "Anonymous"
-    yield Proceed(self, res, **kwargs)
+    return soup
 
 class AuthorFormField:
     introduce_author = StringField("Author") 
@@ -45,19 +35,29 @@ class AuthorModelField:
     introduce_author = db.Column(db.String, nullable=False, default="Anonymous")
 
 class AuthorTemplate:
-    def refine_get(self, original):
-        def get(cls):
-            with weave("flask.render_template", addAuthorCard):
-                return original(cls)
-        
-        return get
+    def refine_render(self, original):
+        @staticmethod
+        def render(filename, **kwargs):
+            with open(f'messageBoard/templates/{filename}') as fp:
+                soup = BeautifulSoup(fp, 'html.parser')
+                soup = add_author_form(soup)
+                soup = add_author_card(soup)
+                
+                with NamedTemporaryFile(suffix=".html", dir="messageBoard/templates") as tfp:
+                    tfp.write(str.encode(soup.prettify()))
+                    tfp.seek(0)
+                    return original(path.basename(tfp.name), **kwargs)
 
-    def refine_post(self, original):
-        def post(cls):
-            with weave(db.session.add, defaultAnon):
-                return original(cls)
+        return render
 
-        return post
+    def refine_add_to_db(self, original):
+        @staticmethod
+        def add_to_db(data):
+            if data["author"] == "":
+                data["author"] = "Anonyous"
+            original(data)
+
+        return add_to_db
 
 def select(composer):
     from messageBoard.models.message import Message
--- examples/flask_example/messageBoard/routes/home.py
+++ examples/flask_example/messageBoard/routes/home.py
@@ -1,10 +1,9 @@
-from flask.views import MethodView
-from flask import Blueprint, request, redirect
+from flask import Blueprint, request
 from wtforms import Form, StringField, TextAreaField
 from wtforms.validators import DataRequired
 from sqlalchemy import select
 
-import flask
+from featurepy.flask import FMethodView
 
 from ..db import db
 from ..models.message import Message
@@ -15,20 +14,25 @@ class MessageForm(Form):
     title = StringField("Title", validators=[DataRequired()])
     message = TextAreaField("Message", validators=[DataRequired()])
 
-class HomeView(MethodView):
+class HomeView(FMethodView):
+    @staticmethod
+    def add_to_db(data):
+        db.session.add(Message(**data))
+        db.session.commit()
 
     def get(self):
         form = MessageForm(request.form)
         messages = db.session.execute(select(Message)).scalars().all()
-        return flask.render_template("home.html", form=form, messages=messages)
+        return self.render("home.html", form=form, messages=messages)
 
     def post(self):
         form = MessageForm(request.form)
         if form.validate():
             print("Form validated")
-            db.session.add(Message(**form.data))
-            db.session.commit()
+            # db.session.add(Message(**form.data))
+            # db.session.commit()
+            self.add_to_db(form.data)
         
-        return redirect("/")
+        return self.redirect("/")
 
 bp.add_url_rule("/", view_func=HomeView.as_view("home"))
